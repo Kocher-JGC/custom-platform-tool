9da3ab6827d21016d71b10905357f240d80a9549
sanget <zh.sanget@gmail.com>
2020-10-27 11:14:50 +0800

    chore: 更新 CD 的配置

--- scripts/build-web-platform.sh
+++ scripts/build-web-platform.sh
@@ -1,12 +1,8 @@
-# 清除工作区
-rm -rf ./dist/web-platform
-
-# 创建文件夹
-mkdir -p ./dist/web-platform
-
 # 保存当前的路径
 pwd=$PWD
 
+source_dir=$pwd/packages/web-platform
+
 # 构建所有项目
-cd "$pwd/packages/web-platform" && yarn build
-#  && mv ./dist/* "$pwd/dist/web-platform"
+cd $source_dir && yarn build
+# && mv ./dist/* "$pwd/dist/web-platform"
