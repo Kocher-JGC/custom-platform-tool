2b2be187dfb587264cf26171bbe9a69b88685f43
Kocher-JGC <879515820@qq.com>
2020-10-28 14:01:12 +0800

    Merge branch 'dev' of 10.0.4.55:custom-platform-v3-frontend-group/custom-platform-v3-frontend into feat/20200812/IUBEngineDemo
    # Conflicts:
    #	packages/web-platform/src/pages/container/index.tsx

--- packages/engine/iub-dsl/demo/base-reference/user/usertable/index.ts
+++ packages/engine/iub-dsl/demo/base-reference/user/usertable/index.ts
@@ -1,9 +1,33 @@
 import {
   FoundationType, ComplexType, TypeOfIUBDSL, Schemas
 } from '@iub-dsl/definition';
+import { FlowCollection } from '@iub-dsl/definition/flow';
+import { ActionCollection } from '@iub-dsl/definition/actions/action';
 import { actionsCollection, demoActionFlow } from './action-collection';
 import { schemas } from './scheams';
 import { componentsCollection } from './components';
+
+export const tableExtralFlow: FlowCollection = {
+  updFlow_1: {
+    id: 'updFlow_1',
+    actionId: 'updA_1',
+    flowOut: [],
+    flowOutCondition: []
+  }
+
+};
+
+export const tableExtralAction: ActionCollection = {
+  updA_1: {
+    actionId: 'updA_1',
+    actionName: 'updA_1_name',
+    actionType: 'openModalFromTableClick',
+    actionOptions: {
+      pageUrl: ''
+    },
+    actionOutput: 'undefined',
+  }
+};
 
 export const userTable = {
   sysRtCxtInterface: {} as any,
