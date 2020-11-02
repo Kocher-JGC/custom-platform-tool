2cf3f4f51336aafcdde2eccfc153140fed91532a
vale <zongyuan-zhang@qq.com>
2020-11-02 12:04:01 +0800

    feat: 应用发布压缩包添加生成应用配置文件

--- packages/web-server/src/release-app/release-app.controller.ts
+++ packages/web-server/src/release-app/release-app.controller.ts
@@ -5,8 +5,6 @@ import { PageDataService } from "../page-data/page-data.service";
 import { ResHelperService } from "../res-helper/res-helper.service";
 import { ReleaseAppService } from "./release-app.service";
 
-const path = require("path");
-
 @Controller("release-app")
 export class ReleaseAppController {
   constructor(
@@ -20,7 +18,7 @@ export class ReleaseAppController {
    * 生成指定应用的配置页面数据压缩包
    * 1. 判断请求参数
    * 2. 根据应用获取发布页面列表
-   * 3. 生成放置 json 临时文件夹
+   * 3. 生成放置 json 临时文件夹，生成页面配置信息 json 文件
    * 4. 循环生成 iub-dsl 数据结构并保存到文件
    * 5. 生成 json 文件压缩包
    * 6. 删除 json 临时文件夹
@@ -28,10 +26,10 @@ export class ReleaseAppController {
    * @returns 生成结果
    * @memberof ReleaseAppController
    */
-  @Get(":applicationCode")
+  @Get("/:lesseeCode/:applicationCode")
   async releaseApp(
   @Res() res: Response,
-    @Param() { applicationCode }: { applicationCode: string }
+    @Param() { lesseeCode, applicationCode }: { lesseeCode: string; applicationCode: string }
   ) {
     const curTime = new Date().getTime();
     const resData = {
@@ -42,18 +40,19 @@ export class ReleaseAppController {
     const {
       getPageDataFromProvider,
       generatePageDataFolder,
+      generateAppConfig,
       generatePageDataJSONFile,
       generatePageDataJSONZip
-      // removePageDataFolder
     } = this.releaseAppService;
     if (applicationCode) {
       let link = "";
-      const folderName = `page-zip-${applicationCode}_${curTime}`;
+      const folderName = `${applicationCode}_${curTime}`;
       const zipName = `${applicationCode}_${curTime}.zip`;
       try {
-        const pageDataRes = await getPageDataFromProvider(applicationCode);
+        const pageDataRes = await getPageDataFromProvider({ lesseeCode, applicationCode });
         if (pageDataRes.length > 0) {
           await generatePageDataFolder(folderName);
+          await generateAppConfig(folderName, { lesseeCode, applicationCode });
           const createAllJSONFileRes = pageDataRes.map(async ({ id, pageContent, dataSources }) => {
             let tableMetaData;
             if (Array.isArray(dataSources) && dataSources.length > 0) {
@@ -74,42 +73,17 @@ export class ReleaseAppController {
             console.log(await resPromise);
           }
           link = await generatePageDataJSONZip(folderName, zipName);
-          console.log("link", link);
           return res.download(link);
         }
         throw new Error("没有页面可以发布");
       } catch (error) {
         console.log(error);
         resData.msg += `生成页面压缩文件失败，${error.toString()}`;
+        return this.resHelperService.wrapResStruct(resData);
       }
     } else {
       resData.msg = "需要参数 app";
+      return this.resHelperService.wrapResStruct(resData);
     }
-    console.log("--- end");
-    return this.resHelperService.wrapResStruct(resData);
-  }
-
-  @Get("/download/:folderName")
-  async downloadApp(@Res() res: Response, @Param() { folderName }: { folderName: string }) {
-    const resData = {
-      data: null,
-      code: this.resHelperService.BusinessCodes.Error,
-      msg: ""
-    };
-    try {
-      if (folderName) {
-        const filePath = await this.releaseAppService.hasFile(folderName);
-        if (filePath) {
-          // TODO 大文件
-          return res.download(filePath);
-        }
-      } else {
-        throw new Error("压缩包不存在");
-      }
-    } catch (error) {
-      console.log(error);
-      resData.msg += `${error.toString()}`;
-    }
-    return this.resHelperService.wrapResStruct(resData);
   }
 }
