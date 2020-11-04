47181716914930d87a481bf43e9672750e3efdef
Kocher-JGC <879515820@qq.com>
2020-11-04 16:08:48 +0800

    feat: 添加页面变量的实现

---
+++ packages/engine/iub-dsl/engine/page-variable/index.ts
@@ -0,0 +1,61 @@
+import { PageVariableOfTable } from "@iub-dsl/definition/page-variable";
+import { PageCommunication } from "@iub-dsl/definition";
+import { RunTimeCtxToBusiness, DispatchModuleName, DispatchMethodNameOfIUBStore } from "../runtime/types";
+
+export const genPageVariableOfTable = ({
+  table,
+  compMark,
+  widgetId
+}, {
+  PK, metaDataRef
+}): PageVariableOfTable => {
+  return {
+    widgetId,
+    compMark,
+    varType: 'TableGrid',
+    valueInfo: {
+      ...table,
+      PK,
+    },
+    metaDataRef,
+  };
+};
+
+export const genPageCommunication = ({
+  pageInfo,
+  metadata,
+  variableData
+}): PageCommunication => {
+  return {
+    pageInfo,
+    metadata,
+    variableData
+  };
+};
+
+const handleF = ({ asyncDispatchOfIUBEngine }: RunTimeCtxToBusiness, pageVar: PageVariableOfTable, metadata) => {
+  const { metaDataRef, valueInfo: { rowData } } = pageVar;
+  console.log(metadata, metaDataRef);
+
+  asyncDispatchOfIUBEngine({
+    dispatch: {
+      module: DispatchModuleName.IUBStore,
+      method: DispatchMethodNameOfIUBStore.updatePageStateFromTableRecord,
+      params: [rowData, metadata[metaDataRef[0]]]
+    }
+  });
+};
+export const pageCommunicationReceiver = (ctx: RunTimeCtxToBusiness, info: PageCommunication) => {
+  const { pageInfo, metadata, variableData } = info;
+  variableData.forEach((item) => {
+    switch (item.varType) {
+      case 'TableCol':
+      case 'TableGrid':
+      case 'TableRow':
+        handleF(ctx, item, metadata);
+        break;
+      default:
+        break;
+    }
+  });
+};
