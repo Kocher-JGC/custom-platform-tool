cdb1fb9861c20f62acd5756c5d323f071f52ea19
Kocher-JGC <879515820@qq.com>
2020-11-04 15:24:17 +0800

    update: 更新demo

--- packages/engine/iub-dsl/demo/base-reference/user/usertable/index.ts
+++ packages/engine/iub-dsl/demo/base-reference/user/usertable/index.ts
@@ -1,5 +1,5 @@
 import {
-  FoundationType, ComplexType, TypeOfIUBDSL, Schemas
+  FoundationType, ComplexType, TypeOfIUBDSL, Schemas, EnumCURD
 } from '@iub-dsl/definition';
 import { FlowCollection } from '@iub-dsl/definition/flow';
 import { ActionCollection } from '@iub-dsl/definition/actions/action';
@@ -11,6 +11,12 @@ export const tableExtralFlow: FlowCollection = {
   updFlow_1: {
     id: 'updFlow_1',
     actionId: 'updA_1',
+    flowOut: [],
+    flowOutCondition: []
+  },
+  delFlow_1: {
+    id: 'delFlow_1',
+    actionId: 'delA_1',
     flowOut: [],
     flowOutCondition: []
   }
@@ -26,6 +32,26 @@ export const tableExtralAction: ActionCollection = {
       pageUrl: ''
     },
     actionOutput: 'undefined',
+  },
+  delA_1: {
+    actionId: 'delA_1',
+    actionName: 'delA_1_name',
+    actionType: 'APBDSLCURD',
+    actionOptions: {
+      businesscode: '34562',
+      actionList: {
+        apb_1: {
+          type: EnumCURD.TableDelete,
+          table: '',
+          condition: {
+            conditionControl: {},
+            conditionList: {}
+          }
+        }
+      },
+      actionStep: ['apb_1'],
+    },
+    actionOutput: 'string',
   }
 };
 
