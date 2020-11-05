5680450816f5dd5b0b3b80212cf6a1a4f18df1b9
lijia <lijia@haoyuntech.com>
2020-11-05 15:49:15 +0800

    feat: 补充权限项、权限树弹窗的回显；以及创建数据后权限树的更新问题

--- packages/provider-app-hub/AuthDistributor/components/AuthShowTree.tsx
+++ packages/provider-app-hub/AuthDistributor/components/AuthShowTree.tsx
@@ -5,12 +5,14 @@ import { getShowAuthoritiesTree } from '../services/apiAgents';
 export const AuthShowTree = (props) => {
   return (
     <AuthTree
+      checkedValues = {props.showAuthItems}
       checkable = {props.checkable || false}
       selectable = {props.selectable || false}
       onSelect = {props.onSelect}
       onRequest = {(searchValue) => {
         return getShowAuthoritiesTree({ name: searchValue });
       }}
+      onRef = {props.onRef}
       nodeConfig = {{
         columnImg: {
           uniqueId: 'id',
