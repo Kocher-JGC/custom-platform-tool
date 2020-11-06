974bc4e091ce4f2010e940226409cf2140732164
sanget <zh.sanget@gmail.com>
2020-11-06 12:16:52 +0800

    chore: 更改上传安装包的路径

--- cd/app_env_config/node-web-app-config.js
+++ cd/app_env_config/node-web-app-config.js
@@ -2,5 +2,6 @@ exports.default = {
   post: 3000,
   host: '0.0.0.0',
   platformApiUrl: 'http://192.168.14.181:6090/paas',
-  mockToken: 'Bearer c2886b8c-b42d-4470-a742-73c9ec479c5c'
+  mockToken: 'Bearer c2886b8c-b42d-4470-a742-73c9ec479c5c',
+  pageDataStoreDirectory: '/tmp/web-platform-update/page-zip'
 };
