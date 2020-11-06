e8ecb3372c306f9f6d19096ad5eb0dd8e7a81b68
Kocher-JGC <879515820@qq.com>
2020-11-06 13:04:42 +0800

    update: 添加应用更新按钮、添加API获取逻辑

--- packages/web-platform/src/utils/utils.ts
+++ packages/web-platform/src/utils/utils.ts
@@ -2,6 +2,7 @@ import {
   history
 } from 'umi';
 import { parse } from 'querystring';
+
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
 
@@ -18,6 +19,7 @@ export const getQueryByParams = (params: string[]) => {
     }
     return item;
   });
+
   return result;
 };
 
