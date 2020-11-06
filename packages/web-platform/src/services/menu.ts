c0981e92e238008f65f9978516edecc1c8beb07f
Kocher-JGC <879515820@qq.com>
2020-11-06 09:53:04 +0800

    update: 内置全局菜单BusinessCode

--- packages/web-platform/src/services/menu.ts
+++ packages/web-platform/src/services/menu.ts
@@ -1,4 +1,4 @@
-import { originGenUrl } from "@/utils/gen-url";
+import { originGenUrl, SYS_MENU_BUSINESSCODE } from "@/utils/gen-url";
 import { getPageQuery } from '@/utils/utils';
 /**
  * 获取用户菜单
@@ -6,7 +6,7 @@ import { getPageQuery } from '@/utils/utils';
  */
 export async function queryMenuList(params: API.IMeunParams) {
   const { app, lessee } = getPageQuery();
-  return $A_R(originGenUrl(lessee, app, "queryPerson"), {
+  return $A_R(originGenUrl(lessee, app, SYS_MENU_BUSINESSCODE), {
     method: 'POST',
     data: {
       steps: [
--- packages/web-platform/src/utils/gen-url.ts
+++ packages/web-platform/src/utils/gen-url.ts
@@ -4,11 +4,14 @@ import { getPageQuery } from './utils';
 // const baseUrl = 'http://192.168.14.140:7091/hy/saas';
 const baseUrl = '';
 // const baseUrl = '/apbdsl';
+
+export const SYS_MENU_BUSINESSCODE = '__system_get_app_menu__';
+
 export const originGenUrl = (lesseeCode, appCode, businessCode) => {
   return `${baseUrl}/${lesseeCode || "hy"}/${appCode || "zxx"}/business/${businessCode}`;
 };
 
-export const getAPBDSLtestUrl = (code = 'queryPerson') => {
+export const getAPBDSLtestUrl = (code = SYS_MENU_BUSINESSCODE) => {
   const { app, lessee } = getPageQuery();
   return originGenUrl(lessee, app, code);
 };
