393f4edea97f7dd765cea23c183669a88439d3b6
sanget <zh.sanget@gmail.com>
2020-11-16 09:04:23 +0800

    chore: 更新 CD 配置

--- scripts/build-consumer-app.sh
+++ scripts/build-consumer-app.sh
@@ -5,9 +5,11 @@ source_dir=$pwd/packages/consumer-app-hub
 
 # 构建 web 服务
 cd $source_dir/web-server
+echo "进入 web-server 构建"
 yarn
 
 # 构建所有项目
 cd $source_dir/web-client
+echo "进入 web-client 构建"
 yarn build
 # && mv ./dist/* "$pwd/dist/web-platform"
