1c815d27f030d11c33d922cf88f49b24e28a99fa
lijia <lijia@haoyuntech.com>
2020-11-19 22:33:03 +0800

    fix: 搜索清空问题

--- packages/provider-app-pages-hub/PageDesigner/app.tsx
+++ packages/provider-app-pages-hub/PageDesigner/app.tsx
@@ -368,7 +368,7 @@ class PageDesignerApp extends React.Component<VisualEditorAppProps & HY.Provider
               flatLayoutItems={flatLayoutItems}
               onReleasePage={this.onReleasePage}
               appLocation={appLocation}
-              ChangeMetadata = {ChangeMetadata}
+              ChangePageMeta = {ChangePageMeta}
             />
           </header>
           <div
