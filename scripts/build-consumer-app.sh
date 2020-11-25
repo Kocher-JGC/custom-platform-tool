8fe297a3cee967e3cedae92a8f38347d42eca8e5
sanget <zh.sanget@gmail.com>
2020-11-25 14:22:57 +0800

    chore: 更新配置

--- scripts/build-consumer-app.sh
+++ scripts/build-consumer-app.sh
@@ -1,13 +1,16 @@
 # 保存当前的路径
 pwd=$PWD
 
+rm -rf node_modules
+rm yarn.lock
+
 source_dir=$pwd/packages/consumer-app-hub
 npm ls @types/react
 
 # 构建 web 服务
 cd $source_dir/web-server
 echo "进入 web-server 构建"
-npm i
+yarn
 # ls node_modules
 
 # 构建所有项目
