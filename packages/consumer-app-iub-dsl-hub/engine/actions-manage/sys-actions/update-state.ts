69871537627d03c253a391278cf493c64da056d3
Kocher-JGC <879515820@qq.com>
2020-11-30 15:34:46 +0800

    update: 更新动作

--- packages/consumer-app-iub-dsl-hub/engine/actions-manage/sys-actions/update-state.ts
+++ packages/consumer-app-iub-dsl-hub/engine/actions-manage/sys-actions/update-state.ts
@@ -1,6 +1,6 @@
 import { UpdateStateOptions } from "@iub-dsl/definition/actions";
 import { ActionDoFn } from "../types";
-import { DispatchModuleName, DispatchMethodNameOfIUBStore } from "../../runtime/types";
+import { DispatchModuleName, DispatchMethodNameOfIUBStore, RunTimeCtxToBusiness } from "../../runtime/types";
 
 /**
  * 最重要的问题: 流程和隔离
@@ -9,64 +9,26 @@ import { DispatchModuleName, DispatchMethodNameOfIUBStore } from "../../runtime/
  * 1. 需要用到标准输入得, 不需要用到标准输入得
  * 2. 配置流程中的上下文的「流程引擎中识别」
  */
-export const updateStateAction = (conf: UpdateStateOptions, baseActionInfo): ActionDoFn => {
+export const changeStateAction = (conf: UpdateStateOptions, baseActionInfo): ActionDoFn => {
   // 更新状态动作的配置和定义
-  const {
-    changeTarget,
-    changeMapping
-  } = conf;
-  /** 在Action接收payload直接点对点改变数据 */
-  if (changeTarget) {
-    return async ({ action, asyncDispatchOfIUBEngine }) => {
-      return await asyncDispatchOfIUBEngine({
-        dispatch: {
-          module: DispatchModuleName.IUBStore,
-          method: DispatchMethodNameOfIUBStore.targetUpdateState,
-          params: [changeTarget, action.payload],
-        },
-        actionInfo: action,
-      });
-    };
-  }
-  /**
-   * 映射结构的改变
-   * from: 表达式, 固定值,
-   * target: 某个页面变量的标志位 「特殊(其他动作的职能): 如:联动 username将转成 @(schema).dId1」
-   * 问题: dId2[1]/sdId2, 层级的描述和转换, 放在哪?
-   */
+  const { changeMapping } = conf;
   if (changeMapping) {
-    // {@(schema).did1: val}
-    return async ({ action, asyncDispatchOfIUBEngine }) => {
-      // const A = {
-      //   did1: 1,
-      //   did2: 2,
-      //   'dataSource[1]/username': 3,
-      //   'department[2]/children[3]/deparmentName': 1,
-      // };
-      const actualChangeMapping = changeMapping.map(async ({ from: oldFrom, target }) => {
-        /** from的转换, 比如表达式的运算 */
-        const from = oldFrom;
-        // const from = await asyncDispatchOfIUBEngine({
-        //   /** 不确定的模块进行模糊调度来求值 */
-        //   dispatch: { // TODO: 未开发
-        //     module: DispatchModuleName.IUBStore,
-        //     method: DispatchMethodNameOfIUBStore.updatePageState,
-        //     params: [oldFrom],
-        //   }
-        // });
-
-        return { from, target };
-      });
-      return await asyncDispatchOfIUBEngine({
-        dispatch: {
-          module: DispatchModuleName.IUBStore,
-          method: DispatchMethodNameOfIUBStore.mappingUpdateState,
-          params: [actualChangeMapping],
-        },
-        actionInfo: action,
-      });
+    return async (ctx: RunTimeCtxToBusiness) => {
+      // const { action: { payload }, asyncDispatchOfIUBEngine } = ctx;
+      // if (payload) {
+      //   asyncDispatchOfIUBEngine({
+      //     dispatch: {
+      //       module: DispatchModuleName.IUBStore,
+      //       method: DispatchMethodNameOfIUBStore.targetUpdateState,
+      //       params: [ changeMapping.struct[0].key, payload]
+      //     }
+      //   });
+      // }
+      console.log(changeMapping);
+      console.log(ctx);
     };
   }
   return async () => {
+    console.log(changeMapping);
   };
 };
--- packages/consumer-app-iub-dsl-hub/engine/actions-manage/types/action.ts
+++ packages/consumer-app-iub-dsl-hub/engine/actions-manage/types/action.ts
@@ -2,32 +2,10 @@ import { AllActionType } from "@iub-dsl/definition/actions/action";
 import { RunTimeCtxToBusiness } from "../../runtime/types";
 
 /**
- * 动作解析扩展的信息
- * experiment 实验性
- */
-export interface ExtralActionParseRes {
-  // actionConf, // ! 尽量不要暴露, 因为actionConf会不安全
-  changeStateToUse: string[];
-  getStateToUse: string[];
-}
-
-/**
- * 基础动作解析返回的信息
- */
-export interface BaseActionInfoParseRes {
-  actionHandle: any; // Func
-}
-
-/**
- * 动作解析的结果
- */
-export type ActionInfoParseRes = BaseActionInfoParseRes & ExtralActionParseRes
-
-/**
  * 动作解析的结果 「列表]
  */
-export interface ActionInfoListParseRes {
-  [actionId: string]: ActionInfoParseRes
+export interface ActionList {
+  [actionId: string]: any; /** AOP, 不知道可以嵌套多少层 */
 }
 
 /**
@@ -35,8 +13,8 @@ export interface ActionInfoListParseRes {
  */
 export interface ActionParserRes {
   actionIds: string[];
-  actionParseRes: ActionInfoListParseRes;
-  getActionParseRes: (actionID: string) => ActionInfoParseRes
+  actionList: ActionList;
+  bindAction: (actionID: string) => ActionDoFn
 }
 
 /**
@@ -49,8 +27,10 @@ export interface BaseActionInfo {
 }
 
 /**
- * 动作解析返回函数的标准
+ * 动作解析返回函数的标准 {TODO: 目前的action都是promise}
  */
 export interface ActionDoFn {
   (ctx: RunTimeCtxToBusiness): Promise<any> // any: 动作返回标准
 }
+
+// experiment 实验性
