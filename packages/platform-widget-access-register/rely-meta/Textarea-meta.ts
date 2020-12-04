4aa852ccc7f391952b81d94e7a822901c2460a87
lijia <lijia@haoyuntech.com>
2020-12-04 21:53:56 +0800

    feat: 补充各控件事件动作

--- packages/platform-widget-access-register/rely-meta/Textarea-meta.ts
+++ packages/platform-widget-access-register/rely-meta/Textarea-meta.ts
@@ -5,6 +5,11 @@ export const TextareaMeta = CreateWidgetPropItemRely({
   label: '多行文本框',
   wGroupType: 'formController',
   widgetRef: 'Textarea',
+  eventAttr: [
+    { alias: '值改变事件', type: 'onChange' },
+    { alias: '鼠标移入事件', type: 'onMouseIn' },
+    { alias: '鼠标移除事件', type: 'onMouseOut' },
+  ],
   varAttr: [{
     alias: '实际值',
     attr: 'realVal',
@@ -26,6 +31,7 @@ export const TextareaMeta = CreateWidgetPropItemRely({
       },
       { propID: 'prop_real_value', editAttr: ['realVal', 'exp', 'variable'] },
       { propID: 'prop_field', editAttr: ['field'] },
+      { propID: 'prop_action_config', editAttr: ['eventRef'] },
     ]
   }
 });
