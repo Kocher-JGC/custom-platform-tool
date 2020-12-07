ad2200d1151082e318a1425d977c9fbfcf8172a6
lijia <lijia@haoyuntech.com>
2020-12-07 14:39:12 +0800

    feat: 用样式类调整标题位置

--- packages/platform-widget-hub/GeneralTable/comp.tsx
+++ packages/platform-widget-hub/GeneralTable/comp.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Table as AntTable } from 'antd';
 import ProTable, { ProColumns, TableDropdown, ActionType } from '@ant-design/pro-table';
-
+import { spawn } from 'child_process';
+import './style.scss';
 export interface GeneralTableCompProps {
   columns: any[]
   dataSource: any[]
   title: string
-  titlePlace: string
+  titlePlace: 'left'|'right'|'center'
 }
 
 export const GeneralTableComp: React.FC<GeneralTableCompProps> = (props) => {
   // console.log(props);
-  const { columns = [], dataSource = [], title, ...other } = props || {};
+  const { columns = [], dataSource = [], title, titlePlace, ...other } = props || {};
   // console.log(columns, dataSource);
   return (
     <div>
       <ProTable 
+        className={`title-place-${titlePlace}`}
         headerTitle={title}
         columns={columns || []} 
         dataSource={dataSource} 
