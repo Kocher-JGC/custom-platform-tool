b6a4ccdbe2bc809f783bc6a6bdd1b1a10db5efaa
lijia <lijia@haoyuntech.com>
2020-12-12 20:18:59 +0800

    feat: 表格支持配置附属表字段

--- packages/widgets-access-workspace/platform-widget-prop-item-hub/TableDSHelper/utils.ts
+++ packages/widgets-access-workspace/platform-widget-prop-item-hub/TableDSHelper/utils.ts
@@ -30,7 +30,7 @@ export const isReferenceField = (dataType) => {
  * 模拟生成 row 数据
  */
 export const genRenderColumn = (usingColumn) => {
-  const { id: fieldID, name: title, tableID: dsID, colDataType } = usingColumn;
+  const { id: fieldID, name: title, dsID, colDataType } = usingColumn;
   const id = `field.dsColumn.${nanoid(8)}`;
   return {
     title, dsID, fieldID,
