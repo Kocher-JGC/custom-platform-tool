cd41d7cf167bb253456fc201abfc98b3f252b99e
vale <zongyuan-zhang@qq.com>
2020-12-14 15:25:13 +0800

    fix: 修改创建应用组件

--- packages/provider-app/provider-app-pages-hub/Dashboard/CreateApp.tsx
+++ packages/provider-app/provider-app-pages-hub/Dashboard/CreateApp.tsx
@@ -1,62 +1,102 @@
-import React from 'react';
-import { FormLayout } from '@deer-ui/core/form-layout';
+import React, { useState } from "react";
+import { Form, Input, Button } from "antd";
 import { CreateApplication } from "@provider-app/services";
 
-export const CreateApp = ({
-  onSuccess
-}) => {
-  return (
-    <div className="create-app">
-      <FormLayout
-        formBtns={[
-          {
-            text: '提交',
-            action: ({ value }) => {
-              console.log('value', value);
-              // const _value = Object.assign({}, value, {
-              //   appShortNameEn: value.appName,
-              //   appShortNameCn: value.appName,
-              // });
-              CreateApplication(value).then(((res) => {
-                onSuccess();
-              }));
-            }
-          }
-        ]}
-        formOptions={[
-          {
-            ref: 'appName',
-            type: 'input',
-            title: '系统名称',
-            required: true
-          },
-          {
-            ref: 'appCode',
-            type: 'input',
-            title: '系统编码',
-            required: true
-          },
-          {
-            ref: 'accessName',
-            type: 'input',
-            title: '系统访问编号',
-            required: true
-          },
+const layout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 6, span: 16 },
+};
 
+export const CreateApp = ({ onSuccess }) => {
+  const [submitting, setSubmitting] = useState(false);
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    if (submitting) return;
+    setSubmitting(true);
+    CreateApplication(values).then((res) => {
+      setSubmitting(false);
+      onSuccess();
+    });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      {...layout}
+      name="createApp"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      style={{ paddingTop: 24 }}
+    >
+      <Form.Item
+        label="系统名称"
+        name="appName"
+        rules={[{ required: true, message: "系统名称必填" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="系统编码"
+        name="appCode"
+        rules={[
           {
-            ref: 'appShortNameCn',
-            type: 'input',
-            title: '系统简称',
-            required: true
-          },
-          {
-            ref: 'appShortNameEn',
-            type: 'input',
-            title: '系统英文简称',
-            required: true
+            required: true,
+            pattern: /^[a-z0-9]+$/,
+            message: "系统编码必填，并且只能为小写字母、数字",
           },
         ]}
-      />
-    </div>
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="系统访问编号"
+        name="accessName"
+        rules={[
+          {
+            required: true,
+            pattern: /^[a-z0-9]+$/,
+            message: "系统访问编号必填，并且只能为小写字母、数字",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="系统简称"
+        name="appShortNameCn"
+        rules={[{ required: true, message: "系统简称必填" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="系统英文简称"
+        name="appShortNameEn"
+        rules={[
+          {
+            required: true,
+            pattern: /^[a-z]+$/,
+            message: "系统英文简称必填，小写字母",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit" loading={submitting}>
+          提交
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
