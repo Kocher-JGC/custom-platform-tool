6e4b503615861e58e46724b9a4e8a2bdcc3293f1
Kocher-JGC <879515820@qq.com>
2020-12-14 16:27:58 +0800

    update: 添加表格select事件

--- packages/consumer-app/consumer-app-iub-dsl-hub/engine/event-manage/normal-event-param/normal-event-param.ts
+++ packages/consumer-app/consumer-app-iub-dsl-hub/engine/event-manage/normal-event-param/normal-event-param.ts
@@ -1,15 +1,28 @@
 import { AllWidgetType, TriggerEventTypes } from "@iub-dsl/definition";
-import { defaultNormal, defaultOnClick, defaultOnChange } from "./default-normal-of-event";
+import { defaultNormal, defaultOnClick, defaultOnChange, genEventBaseConf } from "./default-normal-of-event";
 import { NormalEventParamFn } from "../types";
 
+type EventType = TriggerEventTypes & 'onTableSelect'
+
 /** 默认的标准化事件的函数 */
-const defaultNormalEventFn: { [k in TriggerEventTypes] } = {
+const defaultNormalEventFn: { [k in EventType] } = {
   onChange: defaultOnChange,
   onClick: defaultOnClick,
   onFocus: defaultNormal,
   onMount: defaultNormal,
   onTap: defaultNormal,
   onUnmount: defaultNormal,
+  onTableSelect: (
+    /** widget配置 */
+    widgetConf
+  ) => (e) => {
+    const {selectedRowKeys, selectedRows} = e
+    return {
+      ...genEventBaseConf(widgetConf),
+      payload: {selectedRows, selectedRowKeys},
+      type: 'onTableSelectNormal'
+    }
+  }
 };
 
 /**
