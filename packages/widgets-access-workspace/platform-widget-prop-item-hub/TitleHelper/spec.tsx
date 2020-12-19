91f159e7948e076edd0d754bc34272b9f65a95fd
lijia <lijia@haoyuntech.com>
2020-12-19 18:27:48 +0800

    feat: 改动标题属性

--- packages/widgets-access-workspace/platform-widget-prop-item-hub/TitleHelper/spec.tsx
+++ packages/widgets-access-workspace/platform-widget-prop-item-hub/TitleHelper/spec.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react';
-import { PropItem, PropItemRenderContext } from '@platform-widget-access/spec';
-import { TitleEditorComp } from './comp';
+import React, { useEffect } from "react";
+import { PropItem, PropItemRenderContext } from "@platform-widget-access/spec";
+import { TitleEditorComp } from "./comp";
 
 @PropItem({
-  id: 'prop_title_value',
-  label: '标题',
-  whichAttr: ['title', 'field'],
+  id: "prop_title_value",
+  label: "标题",
+  whichAttr: ["title", "field"],
   defaultValues: {
-    title: '标题'
+    title: "标题",
   },
 })
 export class TitleHelperSpec {
   render(ctx: PropItemRenderContext) {
-    const { takeMeta, changeWidgetType } = ctx.platformCtx.meta;
     return (
       <>
         {/* <div onClick={e => {
@@ -21,7 +20,7 @@ export class TitleHelperSpec {
         >
         更改为下拉框类型
         </div> */}
-        <TitleEditorComp {...ctx} takeMeta={takeMeta} />
+        <TitleEditorComp {...ctx} />
       </>
     );
   }
