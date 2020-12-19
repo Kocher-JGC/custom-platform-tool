c5b150ca54df426bf889ad9505cb708864b050d6
lijia <lijia@haoyuntech.com>
2020-12-19 18:27:48 +0800

    feat: 改动标题属性

--- packages/widgets-access-workspace/platform-widget-prop-item-hub/TitleHelper/comp.tsx
+++ packages/widgets-access-workspace/platform-widget-prop-item-hub/TitleHelper/comp.tsx
@@ -1,37 +1,37 @@
-import React, { useEffect } from 'react';
-import { Input } from '@infra/ui';
-import { PropItemRenderContext } from '@platform-widget-access/spec';
+import React, { useEffect } from "react";
+import { Input } from "@infra/ui";
+import { PropItemRenderContext } from "@platform-widget-access/spec";
 
 export const TitleEditorComp: React.FC<PropItemRenderContext> = ({
   changeEntityState,
   editingWidgetState,
   platformCtx: {
-    meta: {
-      takeMeta
-    }
-  }
+    meta: { takeMeta },
+  },
 }) => {
   const { title, field } = editingWidgetState;
   const selectedField = takeMeta({
-    metaAttr: 'schema',
-    metaRefID: field
+    metaAttr: "schema",
+    metaRefID: field,
   });
   useEffect(() => {
     const nextTitle = selectedField?.column?.name;
     if (!nextTitle || nextTitle === title) return;
     changeEntityState({
-      attr: 'title',
-      value: nextTitle
+      attr: "title",
+      value: nextTitle,
     });
   }, [field]);
   return (
     <div>
       <Input
-        value={title || ''}
-        onChange={(value) => changeEntityState({
-          attr: 'title',
-          value
-        })}
+        value={title || ""}
+        onChange={(value) =>
+          changeEntityState({
+            attr: "title",
+            value,
+          })
+        }
       />
     </div>
   );
