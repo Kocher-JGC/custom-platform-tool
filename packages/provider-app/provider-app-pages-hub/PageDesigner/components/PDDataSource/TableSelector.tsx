5e34b157f90abb465467df89498e903149700ba7
lijia <lijia@haoyuntech.com>
2020-12-21 12:19:21 +0800

    fix: 数据源，字典，的信息获取，需要包含，dictId, childDictId

--- packages/provider-app/provider-app-pages-hub/PageDesigner/components/PDDataSource/TableSelector.tsx
+++ packages/provider-app/provider-app-pages-hub/PageDesigner/components/PDDataSource/TableSelector.tsx
@@ -1,55 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Radio, Tag, Button, Input } from 'antd';
-import { queryTableListService } from '@provider-app/table-structure/service';
-import { ModuleTree } from '../PDInfraUI/ModuleTreeRenderer';
+import React, { useState, useEffect } from "react";
+import { Table, Radio, Tag, Button, Input } from "antd";
+import { queryTableListService } from "@provider-app/table-structure/service";
+import { ModuleTree } from "../PDInfraUI/ModuleTreeRenderer";
 
 interface SelectedRowInfo {
-  id: string
-  name: string
-  type: string
+  id: string;
+  name: string;
+  type: string;
 
-  auxTable?: {containAuxTable: boolean}
+  auxTable?: { containAuxTable: boolean };
 }
 interface TableSelectorProps {
   /** 提交已选中的项 */
-  onSubmit: (selectedRowInfo: SelectedRowInfo[]) => void
-  defaultSelectedInfo?: SelectedRowInfo[]
-  single?: boolean
+  onSubmit: (selectedRowInfo: SelectedRowInfo[]) => void;
+  defaultSelectedInfo?: SelectedRowInfo[];
+  single?: boolean;
 }
-type AuxTableContainer = {[key: string]: boolean}
+type AuxTableContainer = { [key: string]: boolean };
 
 interface TableConfig {
   param: {
-    moduleId?: React.ReactText
-    lastModifiedByMe?: boolean
-    createdByMe?: boolean
-    offset?: number
-    size?: number
-    total?: number
-    name?: string
-  }
-  
-  list: any[]
+    moduleId?: React.ReactText;
+    lastModifiedByMe?: boolean;
+    createdByMe?: boolean;
+    offset?: number;
+    size?: number;
+    total?: number;
+    name?: string;
+  };
+
+  list: { name: string; id: string; hadAuxTable: boolean }[];
 }
-const useTableList = (defaultParam = {
-  offset: 0,
-  size: 10
-}): [TableConfig, (param?: TableConfig['param']) => void] => {
-  
+const useTableList = (
+  defaultParam = {
+    offset: 0,
+    size: 10,
+  }
+): [TableConfig, (param?: TableConfig["param"]) => void] => {
   const [dataList, setList] = useState<TableConfig>({
     param: defaultParam,
-    list: []
+    list: [],
   });
-  const getListByPaging = (param: TableConfig['param'] = defaultParam) => {
-    const { 
-      name = dataList.param.name, 
-      offset = 0, 
-      size = dataList.param.size, 
-      createdByMe = dataList.param.createdByMe, 
-      lastModifiedByMe = dataList.param.lastModifiedByMe, 
-      moduleId = dataList.param.moduleId 
+  const getListByPaging = (param: TableConfig["param"] = defaultParam) => {
+    const {
+      name = dataList.param.name,
+      offset = 0,
+      size = dataList.param.size,
+      createdByMe = dataList.param.createdByMe,
+      lastModifiedByMe = dataList.param.lastModifiedByMe,
+      moduleId = dataList.param.moduleId,
     } = param;
-    queryTableListService({ name, moduleId, createdByMe, lastModifiedByMe, offset, size, addHadAuxTableTag: true }).then((resData) => {
+    queryTableListService({
+      name,
+      moduleId,
+      createdByMe,
+      lastModifiedByMe,
+      offset,
+      size,
+      addHadAuxTableTag: true,
+    }).then((resData) => {
       const { total, data } = resData?.result || {};
       setList({
         param: {
@@ -59,9 +68,9 @@ const useTableList = (defaultParam = {
           lastModifiedByMe,
           offset,
           size,
-          total
+          total,
         },
-        list: data
+        list: data,
       });
     });
   };
@@ -71,113 +80,143 @@ const useTableList = (defaultParam = {
   return [dataList, getListByPaging];
 };
 interface TableListProps extends TableSelectorProps {
-  moduleId?: React.ReactText
+  moduleId?: React.ReactText;
 }
 const TableList: React.FC<TableListProps> = ({
-  single, onSubmit, defaultSelectedInfo = [], moduleId 
+  single,
+  onSubmit,
+  defaultSelectedInfo = [],
+  moduleId,
 }) => {
-  const [{ param: { createdByMe, lastModifiedByMe, name, total }, list }, getTableConfig] = useTableList();
-  const [auxTableContainer, setAuxTableContainer] = useState<AuxTableContainer>({});
+  const [
+    {
+      param: { createdByMe, lastModifiedByMe, name, total },
+      list,
+    },
+    getTableConfig,
+  ] = useTableList();
+  const [auxTableContainer, setAuxTableContainer] = useState<AuxTableContainer>(
+    {}
+  );
   useEffect(() => {
     initAuxTableContainer();
   }, []);
-  useEffect(()=>{
+  useEffect(() => {
     const moduleIdParam = moduleId ? { moduleId } : {};
     getTableConfig(moduleIdParam);
   }, [moduleId]);
   /** 渲染回填数据：是否带入附属表 */
   const initAuxTableContainer = () => {
     const result = {};
-    defaultSelectedInfo.forEach(item=>{
+    defaultSelectedInfo.forEach((item) => {
       const { id, auxTable } = item;
-      result[id] = auxTable && 'containAuxTable' in auxTable ? auxTable.containAuxTable : false;
+      result[id] =
+        auxTable && "containAuxTable" in auxTable
+          ? auxTable.containAuxTable
+          : false;
     });
     setAuxTableContainer(result);
   };
   /** 拼装提交数据 */
   const getSubmitData = (rows) => {
-    return rows.map(item=>{
-      const { id, name } = item;
+    return rows.map((item) => {
+      const { id, name: nameInRow } = item;
       return {
-        id, name, auxTable: {
-          containAuxTable: id in containAuxTableRenderer ? containAuxTableRenderer[id] : true
-        }
+        id,
+        name: nameInRow,
+        auxTable: {
+          containAuxTable:
+            id in containAuxTableRenderer ? containAuxTableRenderer[id] : true,
+        },
       };
     });
   };
-  const containAuxTableRenderer = (id)=>{
+  const containAuxTableRenderer = (id) => {
     return id in auxTableContainer ? auxTableContainer[id] : true;
   };
   return (
     <>
       <div>
         <Button
-          className="mr-4" 
-          type={[createdByMe, lastModifiedByMe].includes(true) ? 'text': 'link'}
-          onClick={()=>{
+          className="mr-4"
+          type={
+            [createdByMe, lastModifiedByMe].includes(true) ? "text" : "link"
+          }
+          onClick={() => {
             getTableConfig({
               createdByMe: false,
               lastModifiedByMe: false,
             });
           }}
-        >所有</Button>
+        >
+          所有
+        </Button>
         <Button
-          className="mr-4" 
-          type={createdByMe===true?'link': 'text'}
-          onClick={()=>{
+          className="mr-4"
+          type={createdByMe === true ? "link" : "text"}
+          onClick={() => {
             getTableConfig({
               createdByMe: true,
               lastModifiedByMe: false,
             });
           }}
-        >我创建的</Button>
+        >
+          我创建的
+        </Button>
         <Button
-          className="mr-4" 
-          type={lastModifiedByMe===true?'link': 'text'}
-          onClick={()=>{
+          className="mr-4"
+          type={lastModifiedByMe === true ? "link" : "text"}
+          onClick={() => {
             getTableConfig({
               createdByMe: false,
               lastModifiedByMe: true,
             });
           }}
-        >我最后修改的</Button>
+        >
+          我最后修改的
+        </Button>
         <span className="flex"></span>
       </div>
-      <Input.Search 
+      <Input.Search
         value={name}
         allowClear
         placeholder="请输入表结构名称"
-        onSearch={nameTmpl=>{
+        onSearch={(nameTmpl) => {
           getTableConfig({
-            name: nameTmpl
+            name: nameTmpl,
           });
         }}
       />
       <Table
         columns={[
           {
-            dataIndex: 'name',
-            key: 'name',
-            title: '表结构名称'
+            dataIndex: "name",
+            key: "name",
+            title: "表结构名称",
           },
           {
-            dataIndex: 'id',
-            key: 'id',
-            title: '带入附属表',
-            render: (_t, _r)=>{
+            dataIndex: "id",
+            key: "id",
+            title: "带入附属表",
+            render: (_t, _r) => {
               return _r.hadAuxTable ? (
-                <Radio.Group 
+                <Radio.Group
                   value={containAuxTableRenderer(_t)}
-                  onChange={(e)=>{
+                  onChange={(e) => {
                     const { value } = e.target;
                     setAuxTableContainer({
                       ...auxTableContainer,
-                      [_t]: value
+                      [_t]: value,
                     });
-                    const itemIndex = defaultSelectedInfo.findIndex(item=>item.id === _t);
-                    if( itemIndex > -1 ){
+                    const itemIndex = defaultSelectedInfo.findIndex(
+                      (item) => item.id === _t
+                    );
+                    if (itemIndex > -1) {
                       const selectedInfo = defaultSelectedInfo.slice();
-                      selectedInfo[itemIndex] = Object.assign(selectedInfo[itemIndex], { auxTable: { containAuxTable: value } });
+                      selectedInfo[itemIndex] = Object.assign(
+                        selectedInfo[itemIndex],
+                        { auxTable: { containAuxTable: value } }
+                      );
                       onSubmit(selectedInfo);
                     }
                   }}
@@ -186,61 +225,63 @@ const TableList: React.FC<TableListProps> = ({
                   <Radio value={false}>否</Radio>
                 </Radio.Group>
               ) : null;
-            }
+            },
           },
         ]}
         rowSelection={{
-          selectedRowKeys: defaultSelectedInfo.map(item=>item.id),
-          type: single ? 'radio' : 'checkbox',
+          selectedRowKeys: defaultSelectedInfo.map((item) => item.id),
+          type: single ? "radio" : "checkbox",
           onChange: (rowKeys, rows) => {
             const submitData = getSubmitData(rows);
             onSubmit(submitData);
           },
         }}
-        onChange = {(pagination)=>{
+        onChange={(pagination) => {
           getTableConfig({
-            offset: (pagination?.current || 0 - 1) * ( pagination.pageSize || 10),
+            offset:
+              (pagination?.current || 0 - 1) * (pagination.pageSize || 10),
             size: pagination.pageSize,
           });
         }}
         pagination={{
-          pageSizeOptions: ['10', '20', '30', '40', '50', '100'],
-          size: 'small',
+          pageSizeOptions: ["10", "20", "30", "40", "50", "100"],
+          size: "small",
           showSizeChanger: true,
           showQuickJumper: true,
-          total: total
+          total,
         }}
         rowKey="id"
         size="small"
         dataSource={list}
-        scroll={{ y: 330 }} 
+        scroll={{ y: 330 }}
       />
     </>
   );
 };
 
-export const SelectedTags = ({
-  onSubmit, defaultSelectedInfo
-}) => {
+export const SelectedTags = ({ onSubmit, defaultSelectedInfo }) => {
   return (
-    <div className="border border-gray-400 border-solid pl-2 pt-2 overflow-auto" style={{ height: 69 }}>
-      { defaultSelectedInfo?.length > 0 ? (
+    <div
+      className="border border-gray-400 border-solid pl-2 pt-2 overflow-auto"
+      style={{ height: 69 }}
+    >
+      {defaultSelectedInfo?.length > 0 ? (
         <Tag
           className="cursor-pointer"
           color="#488CF0"
           style={{ marginBottom: 8 }}
-          onClick={e=>{
+          onClick={(e) => {
             onSubmit([]);
           }}
         >
-        全部清空
+          全部清空
         </Tag>
-      ) : null }
-      {defaultSelectedInfo.map(item=>(
+      ) : null}
+      {defaultSelectedInfo.map((item) => (
         <Tag
-          onClose = {e=>{
-            onSubmit(defaultSelectedInfo.filter(loop=>loop.id !==item.id));
-          }} 
+          onClose={(e) => {
+            onSubmit(defaultSelectedInfo.filter((loop) => loop.id !== item.id));
+          }}
           closable
           key={item.id}
         >
@@ -254,30 +295,31 @@ export const SelectedTags = ({
 export const TableSelector: React.FC<TableSelectorProps> = ({
   defaultSelectedInfo,
   onSubmit,
-  single
+  single,
 }) => {
-  const [moduleId, setModuleId] = useState<React.ReactText>('');
+  const [moduleId, setModuleId] = useState<React.ReactText>("");
   return (
     <div className="flex">
       <div className="w-1/4 box-border pr-1">
-        <ModuleTree onSelect={(selectedModuled)=>{
-          setModuleId(selectedModuled);
-        }}
+        <ModuleTree
+          onSelect={(selectedModuled) => {
+            setModuleId(selectedModuled);
+          }}
         />
       </div>
       <div className="w-3/4">
-        <TableList 
+        <TableList
           moduleId={moduleId}
-          defaultSelectedInfo = {defaultSelectedInfo}
+          defaultSelectedInfo={defaultSelectedInfo}
           single={single}
-          onSubmit = {onSubmit}
+          onSubmit={onSubmit}
         />
-        { !single ? (
-          <SelectedTags 
-            defaultSelectedInfo = {defaultSelectedInfo}
-            onSubmit = {onSubmit}
+        {!single ? (
+          <SelectedTags
+            defaultSelectedInfo={defaultSelectedInfo}
+            onSubmit={onSubmit}
           />
-        ) : null }
+        ) : null}
       </div>
     </div>
   );
