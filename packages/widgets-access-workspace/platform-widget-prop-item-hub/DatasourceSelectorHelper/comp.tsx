81614eebef36cae9aa6beba02fbef9cfb744a66d
lijia <lijia@haoyuntech.com>
2020-12-21 11:36:33 +0800

    feat: 下拉框组件，值字段，显示字段，属性开发

--- packages/widgets-access-workspace/platform-widget-prop-item-hub/DatasourceSelectorHelper/comp.tsx
+++ packages/widgets-access-workspace/platform-widget-prop-item-hub/DatasourceSelectorHelper/comp.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Radio } from "antd";
 import { PropItemRenderContext } from "@platform-widget-access/spec";
 import { PD } from "@provider-app/page-designer/types";
+import { DropdownWrapper, Input } from "@infra/ui";
 
 interface OptionsType {
   type: "TABLE" | "DICT";
@@ -43,7 +44,7 @@ export const OptionsSelector: React.FC<OptionsSelectorProps> = (props) => {
 
   const dsBinder = (
     <div
-      className="px-4 py-2 border cursor-pointer"
+      className="__label bg_default t_white cursor-pointer w-full mb10"
       onClick={(e) => {
         platformCtx.selector.openDatasourceSelector({
           defaultSelected: datasourceMeta ? [datasourceMeta] : [],
@@ -63,12 +64,25 @@ export const OptionsSelector: React.FC<OptionsSelectorProps> = (props) => {
               data: bindedDS,
               // metaID:
             });
-            changeEntityState({
-              attr: whichAttr,
-              value: nextMetaID,
-            });
-            // console.log(submitData);
-
+            const changeList = [
+              {
+                attr: whichAttr,
+                value: nextMetaID,
+              },
+            ];
+            /** 需要相应清空 值字段，显示字段 */
+            const newDsID = bindedDS.id;
+            const oldDsID = editingWidgetState[whichAttr];
+            if (newDsID !== oldDsID) {
+              changeList.push(
+                {
+                  attr: "realValField",
+                  value: "",
+                },
+                { attr: "showValField", value: "" }
+              );
+            }
+            changeEntityState(changeList);
             close();
           },
         });
@@ -78,6 +92,11 @@ export const OptionsSelector: React.FC<OptionsSelectorProps> = (props) => {
     </div>
   );
 
+  /**
+   * 获取排序字段的展示信息
+   * @param dsInMeta
+   * @param sortInfo
+   */
   const takeSortInfo = (dsInMeta: PD.TableDatasouce[], sortInfo) => {
     const constructDs = () => {
       const result = {};
@@ -137,11 +156,111 @@ export const OptionsSelector: React.FC<OptionsSelectorProps> = (props) => {
       </div>
     );
   };
-
+  const takeDictField = (dsMeta, fieldCode) => {
+    if (!dsMeta) return "";
+    const value = editingWidgetState[fieldCode];
+    if (!value) return "点击进行字段配置";
+    return dsMeta.columns[value]?.name || "";
+  };
+  /**
+   * 值/显示 字段的渲染器
+   */
+  const renderField = ({ fieldCode, fieldTitle, defaultFieldInDict }) => {
+    if (!datasourceMeta) return null;
+    return (
+      <div className="mb10">
+        <div className="label mb5">{fieldTitle}</div>
+        <div className="content">
+          {datasourceMeta.type === "DICT" ? (
+            <span className="__label bg_default t_white w-full">
+              {defaultFieldInDict}
+            </span>
+          ) : (
+            <DropdownWrapper
+              className=" w-full"
+              outside
+              overlay={(helper) => {
+                return (
+                  <div className="column-selector-container">
+                    {Object.values(datasourceMeta.columns || {}).map(
+                      (col, idx) => {
+                        const { name, id } = col;
+                        const isSelected = editingWidgetState[fieldCode] === id;
+                        return (
+                          <div
+                            onClick={() => {
+                              changeEntityState({
+                                attr: fieldCode,
+                                value: id,
+                              });
+                              helper.hide();
+                            }}
+                            className={`p1-1 list-item ${
+                              isSelected ? "disabled" : ""
+                            }`}
+                            key={id}
+                          >
+                            {`${name}`}
+                          </div>
+                        );
+                      }
+                    ) || null}
+                  </div>
+                );
+              }}
+            >
+              <span className="__label bg_default t_white cursor-pointer w-full">
+                {takeDictField(datasourceMeta, fieldCode)}
+              </span>
+            </DropdownWrapper>
+          )}
+        </div>
+      </div>
+    );
+  };
+  /**
+   * 默认值 的渲染器
+   */
+  const renderDefaultValue = () => {
+    if (!datasourceMeta) return null;
+    return (
+      <div className="mb10">
+        <div className="label mb5">默认值</div>
+        <div className="content">
+          {datasourceMeta.type === "DICT" ? (
+            <span className="__label bg_default t_white w-full">
+              {defaultFieldInDict}
+            </span>
+          ) : (
+            <Input
+              className="w-full"
+              value={editingWidgetState.realValDefault}
+              onChange={(value) => {
+                changeEntityState({
+                  attr: "realValDefault",
+                  value,
+                });
+              }}
+            />
+          )}
+        </div>
+      </div>
+    );
+  };
   return (
     <>
       <div>{dsBinder}</div>
       {renderSortList()}
+      {renderField({
+        fieldCode: "showValField",
+        fieldTitle: "显示字段",
+        defaultFieldInDict: "字典项名称",
+      })}
+      {renderField({
+        fieldCode: "realValField",
+        fieldTitle: "值字段",
+        defaultFieldInDict: "字典项编码",
+      })}
     </>
   );
 };
